0c925d24057f7d291afd6a110cdec1accbabda6b
Alex Tavella <alextavella@outlook.com>
2019-07-04 14:32:27 -0300

    Add Sentry

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
+import path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
-import path from 'path';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as Sentry from '@sentry/node';
+import sentryConfig from './config/sentry';
+import 'express-async-errors'; //  before routes
+// eslint-disable-next-line import/order
+import Youch from 'youch';
 
 import routes from './routes';
 import './database';
@@ -9,11 +16,15 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(bodyParser.urlencoded({ extended: false }));
     this.server.use(bodyParser.json());
     this.server.use(
@@ -24,6 +35,15 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://98bbdc1f16214878b598c05809835cab@sentry.io/1497327',
+};
