06768a38fbe774e06d4d7763050e81a61fa151f8
Alex Tavella <alextavella@outlook.com>
2019-08-29 11:40:12 -0300

    Add avatar

--- src/app/validations/SessionValidation.js
+++ src/app/validations/SessionValidation.js
@@ -1,13 +1,23 @@
 import jwt from 'jsonwebtoken';
 
 import User from '../models/User';
+import File from '../models/File';
 import authConfig from '../../config/auth';
 
 class SessionValidation {
   async login(req, res, next) {
     const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     if (!user) {
       return res.status(401).json({ error: 'User not found.' });
@@ -17,13 +27,15 @@ class SessionValidation {
       return res.status(401).json({ error: 'Password does not match.' });
     }
 
-    const { id, name } = user;
+    const { id, name, avatar, provider } = user;
 
     req.user = {
       user: {
         id,
         name,
         email,
+        provider,
+        avatar,
       },
       token: {
         accessToken: jwt.sign({ id }, authConfig.secret, {
